1220ebdcf3d839ee81e08e4484d763352550637c
Amin-KB <aminkarambeigi94@gmail.com>
2022-09-12 11:54:10 +0200

    first commit

---
+++ WebApplicationPlayground/WebApplicationPlayground/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplicationPlayground.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
